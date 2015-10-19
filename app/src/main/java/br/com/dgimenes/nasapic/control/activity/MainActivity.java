b8d32cacccad64a06af1b26834485464f0e13908
Daniel Gimenes <danielcgimenes@gmail.com>
2015-10-18 22:30:53 -0200

    inverting action menu items

--- app/src/main/java/br/com/dgimenes/nasapic/control/activity/MainActivity.java
+++ app/src/main/java/br/com/dgimenes/nasapic/control/activity/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_picture, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
