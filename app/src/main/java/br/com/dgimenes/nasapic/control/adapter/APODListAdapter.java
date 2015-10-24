f1d47e632f878c8dca4e1ad10c65d67fcb9d3a84
Daniel Gimenes <danielcgimenes@gmail.com>
2015-10-24 00:00:20 -0200

    a first design of the card views (still with mock data)

--- app/src/main/java/br/com/dgimenes/nasapic/control/adapter/APODListAdapter.java
+++ app/src/main/java/br/com/dgimenes/nasapic/control/adapter/APODListAdapter.java
@@ -1,25 +1,36 @@
 package br.com.dgimenes.nasapic.control.adapter;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
 import br.com.dgimenes.nasapic.R;
 import br.com.dgimenes.nasapic.model.APOD;
+import br.com.dgimenes.nasapic.service.DefaultPicasso;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class APODListAdapter extends RecyclerView.Adapter<APODListAdapter.ViewHolder> {
 
+    private final Picasso picasso;
     private List<APOD> dataset;
+    private WeakReference<Context> contextWeak;
 
     private static final String APOD_DATE_FORMAT = "dd/MMM/yyyy";
     private static SimpleDateFormat dateFormatter = new SimpleDateFormat(APOD_DATE_FORMAT);
+    private APODListAdapter.ErrorListener errorListener;
+    private int listWidth;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -29,17 +40,30 @@ public class APODListAdapter extends RecyclerView.Adapter<APODListAdapter.ViewHo
         @Bind(R.id.title)
         public TextView titleTextView;
 
-        @Bind(R.id.url)
-        public TextView urlTextView;
+        @Bind(R.id.explanation)
+        public TextView explanationTextView;
 
-        public ViewHolder(View rootView) {
-            super(rootView);
-            ButterKnife.bind(this, rootView);
+        @Bind(R.id.apod_preview_image)
+        public ImageView apodPreviewImageView;
+
+        public ViewHolder(View cardView) {
+            super(cardView);
+            ButterKnife.bind(this, cardView);
         }
     }
 
-    public APODListAdapter(List<APOD> dataset) {
+    public APODListAdapter(Context context, List<APOD> dataset, ErrorListener errorListener,
+                           int listWidth) {
         this.dataset = dataset;
+        this.errorListener = errorListener;
+        this.listWidth = listWidth;
+        this.picasso = DefaultPicasso.get(context, new Picasso.Listener() {
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                displayErrorMessage(R.string.error_downloading_apod);
+            }
+        });
+        this.contextWeak = new WeakReference<>(context);
     }
 
     @Override
@@ -53,8 +77,35 @@ public class APODListAdapter extends RecyclerView.Adapter<APODListAdapter.ViewHo
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         APOD apod = dataset.get(position);
         viewHolder.dateTextView.setText(dateFormatter.format(apod.getDate()));
-        viewHolder.titleTextView.setText(apod.getTitle());
-        viewHolder.urlTextView.setText(apod.getUrl());
+        viewHolder.titleTextView.setText(limitString(apod.getTitle(), 28));
+        viewHolder.explanationTextView.setText(addQuotes(limitString(apod.getExplanation(), 160)));
+        viewHolder.apodPreviewImageView.setTag(apod.getUrl());
+    }
+
+    private String limitString(String text, int maxCharacters) {
+        if (maxCharacters < text.length()) {
+            return text.substring(0, maxCharacters - 3) + "...";
+        } else {
+            return text;
+        }
+    }
+
+    private String addQuotes(String text) {
+        return "\"" + text + "\"";
+    }
+
+    @Override
+    public void onViewAttachedToWindow(ViewHolder viewHolder) {
+        String src = (String) viewHolder.apodPreviewImageView.getTag();
+        picasso.load(src).placeholder(R.drawable.loading).resize(listWidth, 0)
+                .into(viewHolder.apodPreviewImageView);
+    }
+
+    private void displayErrorMessage(int errorMessageResource) {
+        if (errorListener != null) {
+            String errorMessage = contextWeak.get().getResources().getString(errorMessageResource);
+            errorListener.error(errorMessage);
+        }
     }
 
     @Override
@@ -62,6 +113,7 @@ public class APODListAdapter extends RecyclerView.Adapter<APODListAdapter.ViewHo
         return dataset.size();
     }
 
-
-
+    public interface ErrorListener {
+        void error(String errorMessage);
+    }
 }
--- app/src/main/java/br/com/dgimenes/nasapic/control/fragment/RecentPicturesFragment.java
+++ app/src/main/java/br/com/dgimenes/nasapic/control/fragment/RecentPicturesFragment.java
@@ -1,13 +1,18 @@
 package br.com.dgimenes.nasapic.control.fragment;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,7 +25,7 @@ import br.com.dgimenes.nasapic.model.APOD;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class RecentPicturesFragment extends Fragment {
+public class RecentPicturesFragment extends Fragment implements APODListAdapter.ErrorListener {
 
     @Bind(R.id.recent_pics_recycler_view)
     RecyclerView recyclerView;
@@ -44,16 +49,17 @@ public class RecentPicturesFragment extends Fragment {
         recyclerViewLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(recyclerViewLayoutManager);
 
-
         List<APOD> apods = new ArrayList<>();
         try {
-            apods.add(new APOD("url 1", "explanation 1", "title 1", new SimpleDateFormat("dd/MM/yyyy").parse("22/10/2015")));
-            apods.add(new APOD("url 2", "explanation 2", "title 2", new SimpleDateFormat("dd/MM/yyyy").parse("21/10/2015")));
-            apods.add(new APOD("url 3", "explanation 3", "title 3", new SimpleDateFormat("dd/MM/yyyy").parse("20/10/2015")));
+            APOD apod1 = new APOD("http://apod.nasa.gov/apod/image/1510/IC2118_A_full900.jpg", "Double, double toil and trouble; Fire burn, and cauldron bubble .... maybe Macbeth should have consulted the Witch Head Nebula. A frighteningly shaped reflection nebula, this cosmic crone is about 800 light-years away though. Its malevolent visage seems to glare toward nearby bright star Rigel in Orion, just off the right edge of this frame. More formally known as IC 2118, the interstellar cloud of dust and gas is nearly 70 light-years across, its dust grains reflecting Rigel's starlight. In this composite portrait, the nebula's color is caused not only by the star's intense bluish light but because the dust grains scatter blue light more efficiently than red. The same physical process causes Earth's daytime sky to appear blue, although the scatterers in planet Earth's atmosphere are molecules of nitrogen and oxygen.", "The Witch Head Nebula", new SimpleDateFormat("dd/MM/yyyy").parse("31/10/2015"));
+            apods.add(apod1);
+            apods.add(apod1);
+            apods.add(apod1);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        recyclerViewAdapter = new APODListAdapter(apods);
+
+        recyclerViewAdapter = new APODListAdapter(getActivity(), apods, this, getDisplayWidth());
         recyclerView.setAdapter(recyclerViewAdapter);
 
 //        setWallpaperButton.setOnClickListener(
@@ -80,5 +86,18 @@ public class RecentPicturesFragment extends Fragment {
 //                    }
 //                }
 //        );
+    }
+
+    @Override
+    public void error(String errorMessage) {
+        Snackbar.make(recyclerView, errorMessage, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public int getDisplayWidth() {
+        Display display = ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE))
+                .getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.x;
     }
 }
