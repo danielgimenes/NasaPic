d2b7a159f46b4677c3261683459dbaa118d58934
Daniel Gimenes <danielcgimenes@gmail.com>
2015-11-01 22:42:47 -0200

    adding APOD data to detail screen (ImageZoomActivity)

--- app/src/main/java/br/com/dgimenes/nasapic/control/activity/ImageZoomActivity.java
+++ app/src/main/java/br/com/dgimenes/nasapic/control/activity/ImageZoomActivity.java
@@ -2,10 +2,13 @@ package br.com.dgimenes.nasapic.control.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
@@ -14,6 +17,7 @@ import com.squareup.picasso.Picasso;
 import br.com.dgimenes.nasapic.R;
 import br.com.dgimenes.nasapic.model.APOD;
 import br.com.dgimenes.nasapic.service.DefaultPicasso;
+import br.com.dgimenes.nasapic.util.DateUtils;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -27,6 +31,15 @@ public class ImageZoomActivity extends AppCompatActivity {
     @Bind(R.id.progress_bar)
     ProgressBar progressBar;
 
+    @Bind(R.id.explanation)
+    public TextView explanationTextView;
+
+    @Bind(R.id.open_explanation_button)
+    Button openExplanationButton;
+
+    @Bind(R.id.main_toolbar)
+    Toolbar mainToolbar;
+
     private APOD apod;
 
     @Override
@@ -38,7 +51,30 @@ public class ImageZoomActivity extends AppCompatActivity {
         }
         ButterKnife.bind(this);
         apod = getIntent().getExtras().getParcelable(ImageZoomActivity.APOD_OBJECT_PARAM);
+        setupUI();
         loadImageAsync();
+    }
+
+    private void setupUI() {
+        this.setTitle(apod.getTitle());
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        explanationTextView.setText(DateUtils.friendlyDateString(this, apod.getDate()) + " - \"" +
+                apod.getExplanation() + "\" " + getResources().getString(R.string.text_origin));
+        openExplanationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (explanationTextView.getVisibility() == View.GONE) {
+                    explanationTextView.setVisibility(View.VISIBLE);
+                    openExplanationButton.setText(
+                            getResources().getString(R.string.close_description_button));
+                } else {
+                    explanationTextView.setVisibility(View.GONE);
+                    openExplanationButton.setText(
+                            getResources().getString(R.string.open_description_button));
+                }
+            }
+        });
     }
 
     private void loadImageAsync() {
